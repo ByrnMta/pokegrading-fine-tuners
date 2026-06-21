890355e7b055eac496a0d4a62b7f8ac95f4b0444
Esteban Chinchilla <80222014+elesteban-os@users.noreply.github.com>
2026-06-20 18:29:24 -0600

    Refactorización de diseño de código. (#22)
    * Refactor de código para mejor legibilidad
    
    * Refactor en la forma que se accede los archivos
    
    Ahora los archivos de imágenes y embeddings se acceden desde de "ArchivosRepositorios.py"
    
    * Carpeta "catalogo" en servicios/utilidades

--- backend/Servicios/logica/Grading_Submitter.py
+++ backend/Servicios/logica/Grading_Submitter.py
@@ -9,7 +9,7 @@ from sqlalchemy.orm import Session
 
 from AccesoDatos.CartasRepositorio import CartasRepositorio
 from Modelos import Cartas as models
-from Servicios.utilidades.BuscadorImagenes import EmbeddingService
+from Servicios.utilidades.catalogo.EmbeddingService import EmbeddingService
 
 
 @dataclass

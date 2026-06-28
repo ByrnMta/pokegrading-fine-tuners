7740d68e9a802c7edef9e4f723a61b753d874137
Ludwin Ramos <lujorabri@estudiantec.cr>
2026-06-28 17:01:20 -0600

    Se crea un exception handler y se realiza la validación de existencia de atributos en API B2B

--- backend/Controladores/API_B2B_Controlador.py
+++ backend/Controladores/API_B2B_Controlador.py
@@ -3,6 +3,7 @@ from Datos.db_session import get_db
 from sqlalchemy.orm import Session
 from Esquemas.CartasEsquema import CartaConsultaB2B
 from Servicios.logica.API_B2BServicio import API_B2BServicio
+from Servicios.validaciones.API_B2BValidacion import API_B2BValidacion
 
 router = APIRouter(prefix="/B2B", tags=["b2b"])
 
@@ -18,18 +19,20 @@ def buscar_cartas_catalogo_b2b(
     ):
     
     cartas_consultadas = []
-
     # Se colocan los datos de las cartas en el formato interno
     for carta in lista_cartas_consultar:
-        carta_b2b = CartaConsultaB2B(
-            set_name=carta.get("set_name"),
-            numero=carta.get("numero"),
-            edicion=carta.get("edicion"),
-            idioma=carta.get("idioma"),
-            acabado=carta.get("acabado")
-        )
-        cartas_consultadas.append(carta_b2b)
-    
+        try:
+            carta_b2b = CartaConsultaB2B(
+                set_name=carta["set_name"],
+                numero=carta["numero"],
+                edicion=carta["edicion"],
+                idioma=carta["idioma"],
+                acabado=carta["acabado"]
+            )
+            cartas_consultadas.append(carta_b2b)
+        except KeyError as e:
+            raise HTTPException(status_code=400, detail=f"Campo obligatorio faltante: {e.args[0]}")
+
     # Se llama al servicio para hacer la consulta de las cartas en el catálogo
     resultado = API_B2BServicio.consultar_catalogo_b2b(
         db=db, 
